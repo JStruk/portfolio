bdb5108234274f11c4d66483004fa00246b16477
Justin Struk <j.struk@vehikl.com>
2020-11-02 15:10:33 -0500

    add footer and change image styling

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="footer">
+    <div class="w-full flex">
+
+      <ul>
+<!--        <li v-for="socialLink in socialLinks" :key="socialLink">-->
+<!--          {{socialLink.name}}-->
+<!--          <a :href=socialLink.url>-->
+<!--            <v-icon>mdi-github</v-icon>-->
+<!--          </a>-->
+
+<!--        </li>-->
+        <li>
+
+        </li>
+      </ul>
+
+    </div>
+  </section>
+</template>
+
+
+<script>
+
+import socialLinksJson from '../data/socials.json';
+
+export default {
+
+  data() {
+    return {
+      socialLinks: socialLinksJson.socialLinks
+    }
+  }
+}
+
+</script>
